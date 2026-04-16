45e4f250371a37ffe2cef1f64ae5d801c0c0c058
maxupCDS <chleotjq1@future-link.ai>
2026-04-16 21:50:51 +0900

    sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (e) => {
+  // 앱 설치 조건 충족을 위한 최소한의 코드
+});
